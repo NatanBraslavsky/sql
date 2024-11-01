e8d67d6127100cf2d187de4a56b7cf15af10bba4
Natan Pereira Braslavsky de Oliveira <natanbraslavsky1@gmail.com>
2024-11-01 09:54:08 -0300

    Update revisao.sql

--- revisao_prova/revisao.sql
+++ revisao_prova/revisao.sql
@@ -26,3 +26,80 @@ PRIMARY KEY(codcli, coddep),
 FOREIGN KEY(codcli) REFERENCES cliente (codcli), 
 FOREIGN KEY(coddep) REFERENCES dependente (coddep), 
 );
+
+--ex01
+SELECT nomecliente, genero
+FROM cliente;
+
+--ex02
+SELECT nomeproduto, preco
+FROM produto;
+
+--ex03
+SELECT idcompra, dtcompra
+FROM compras;
+
+--ex04
+SELECT nomecliente, estadocivil, cidade
+FROM cliente;
+
+--ex05
+SELECT * FROM cliente;
+
+--ex06
+SELECT nomecliente, estadocivil
+FROM cliente
+ORDER BY nomecliente ASC;
+
+--ex07
+SELECT nomeproduto, preco
+FROM produto
+ORDER BY preco DESC
+LIMIT 3;
+
+--ex08
+SELECT nomecliente, cidade, estado
+FROM cliente
+ORDER BY cidade,estado;
+
+--ex09
+SELECT DISTINCT cidade
+FROM cliente
+ORDER BY cidade ;
+
+--ex10
+SELECT DISTINCT dtcompra
+FROM compras
+ORDER BY dtcompra;
+
+--ex11
+SELECT nomeproduto
+FROM produto
+WHERE preco > 5;
+
+--ex12
+SELECT nomecliente, estadocivil
+FROM cliente
+WHERE genero = 'f' OR genero = 'F';
+
+--ex13
+SELECT nomecliente, cidade
+FROM cliente
+WHERE estado = 'Rio de Janeiro';
+
+--ex14
+SELECT nomecliente
+FROM cliente
+WHERE dtnascimento < '2004-01-01';
+
+--ex15
+SELECT DISTINCT idcliente
+FROM compras
+WHERE dtcompra BETWEEN '2021-01-01' AND '2021-12-31';
+
+--ex16
+SELECT DISTINCT idcliente
+FROM compras
+WHERE EXTRACT(YEAR FROM)
+
+
